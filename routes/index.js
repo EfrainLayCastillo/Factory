ee4a7e84c8a01f47fefca9f67dc369dd58fb8707
BstDragonLay <efrainlaycastillo@hotmail.com>
2017-05-07 01:12:38 -0500

    fixed it merged

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  res.render('index', { title: 'Store!' });
-=======
   res.render('index', { title: 'Dog\s Creed - STORE!' });
->>>>>>> a93e24e1535105b550ca0157354d3ad8032a3417
-=======
-  res.render('index', { title: 'Dog\s Creed - STORE!' });
->>>>>>> a93e24e1535105b550ca0157354d3ad8032a3417
 });
 
 module.exports = router;
