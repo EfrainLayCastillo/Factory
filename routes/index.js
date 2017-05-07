bd27b6ae0ce5f0dd42ec4757466861195c0558eb
Ubuntu <ubuntu@ip-172-31-28-66.us-west-2.compute.internal>
2017-05-07 05:57:14 +0000

    merge2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+<<<<<<< HEAD
   res.render('index', { title: 'Store!' });
+=======
+  res.render('index', { title: 'Dog\s Creed - STORE!' });
+>>>>>>> a93e24e1535105b550ca0157354d3ad8032a3417
 });
 
 module.exports = router;
