479618168ecb7e56a1063c3a24f6095243cd4909
BstDragonLay <efrainlaycastillo@hotmail.com>
2017-05-07 00:29:54 -0500

    alpha store

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
-var mongoose = require('mongoose')
+//handlebars engine
+var hbs = require('express-handlebars');
+//var mongoose = require('mongoose')
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 //DB server connection
-mongoose.connect('mongodb://localhost/test');
+//mongoose.connect('mongodb://localhost/test');
 
+
+//Engine of Handlebars
+app.engine('.hbs', hbs({
+  extname:'.hbs',
+  defaultLayout:'main',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials'
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
